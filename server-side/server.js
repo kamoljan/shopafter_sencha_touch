21cc617c4382075163c846036299e9300db51660
Kamol Mavlonov <kamol@701search.com>
2013-10-23 15:09:57 +0800

    Added some log into server-side code

--- server-side/server.js
+++ server-side/server.js
@@ -2,10 +2,15 @@ var express = require('express'),
     http = require('http'),
     path = require('path'),
     crypto = require('crypto'),
-    app = express(),
-    bucket = "img.shopafter.com",
-    awsKey = "AKIAJVYDZS3ZM5UT4V4A",
-    secret = "85Z8L0v5DlnLV6UWnXCUbxeE7Sak1JpXMjxNcg";
+    app = express();
+
+var aws = {
+        "accessKeyId": "AKIAJVYDZS3ZM5UT4V4A",
+        "secretAccessKey": "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",
+        "region": "ap-southeast-1",
+        "s3_bucket": "img.shopafter.com",
+        "sslEnabled": false
+    };
 
 app.use(express.logger("dev"));
 app.use(express.methodOverride());
@@ -17,20 +22,31 @@ function sign(req, res, next) {
     var fileName = req.body.fileName,
         expiration = new Date(new Date().getTime() + 1000 * 60 * 5).toISOString(); // expire in 5 minutes
 
+    console.log('expiration = ' + expiration);
+    console.log('fileName = ' + fileName);
+
     var policy =
     {
         "expiration": expiration,
         "conditions": [
-            {"bucket": bucket},
+            {"bucket": aws.s3_bucket},
             {"key": fileName},
-            {"acl": 'public-read'},
+            {"acl": "public-read"},
             ["starts-with", "$Content-Type", ""],
             ["content-length-range", 0, 2097152000]  // 512K*4
         ]
     };
     policyBase64 = new Buffer(JSON.stringify(policy), 'utf8').toString('base64');
-    signature = crypto.createHmac('sha1', secret).update(policyBase64).digest('base64');
-    res.json({bucket: bucket, awsKey: awsKey, policy: policyBase64, signature: signature});
+    signature = crypto.createHmac('sha1', aws.secretAccessKey).update(policyBase64).digest('base64');
+    res.json({
+        region: aws.region,
+        bucket: aws.s3_bucket,
+        awsKey: aws.accessKeyId,
+        policy: policyBase64,
+        signature: signature
+    });
+
+    console.log(JSON.stringify(res));
 }
 
 // FIXME: SECURE THIS ENDPOINT WITH APPROPRIATE AUTHENTICATION/AUTHORIZATION MECHANISM
