d00e83ac13cb9d5daa7ab094bfc0d1867654fd38
Kamol Mavlonov <kamol@701search.com>
2013-10-24 22:31:24 +0800

    Reduced pagesize to 10

--- app/view/AdsListView.js
+++ app/view/AdsListView.js
@@ -77,7 +77,7 @@ Ext.define('ShopAfter.view.AdsListView', {
                 model: "ShopAfter.model.Ad",
                 autoLoad: this.getAutoLoad() === true,
                 remoteFilter: true,
-                pageSize: 20,
+                pageSize: 10,
                 proxy: this.getProxy()
             });
         }
@@ -90,11 +90,12 @@ Ext.define('ShopAfter.view.AdsListView', {
                 '<div class="ad">',
                 '<div class="title">{description}</div>',
                 '<div class="img" style="background-image: url(\'{image}\')"></div>',
-                '<div class="ratings">',
-                '<div class="user<tpl if=\"ratings.audience_score &gt; 60\"> success</tpl><tpl if=\"ratings.audience_score &lt; 0\"> unknown</tpl>"><tpl if=\"ratings.audience_score &gt;= 0\">{ratings.audience_score}<tpl else>&nbsp;</tpl></div>',
+//                '<div class="ratings">',
+                '<div class="user">${price}</div>',
+//                '<div class="user<tpl if=\"ratings.audience_score &gt; 60\"> success</tpl><tpl if=\"ratings.audience_score &lt; 0\"> unknown</tpl>"><tpl if=\"ratings.audience_score &gt;= 0\">{ratings.audience_score}<tpl else>&nbsp;</tpl></div>',
                 '<div class="spacer"></div>',
-                '<div class="critics<tpl if=\"ratings.critics_score &gt; 60\"> success</tpl><tpl if=\"ratings.critics_score &lt; 0\"> unknown</tpl>"><tpl if=\"ratings.critics_score &gt;= 0\">{ratings.critics_score}<tpl else>&nbsp;</tpl></div>',
-                '</div>',
+//                '<div class="critics<tpl if=\"ratings.critics_score &gt; 60\"> success</tpl><tpl if=\"ratings.critics_score &lt; 0\"> unknown</tpl>"><tpl if=\"ratings.critics_score &gt;= 0\">{ratings.critics_score}<tpl else>&nbsp;</tpl></div>',
+//                '</div>',
                 '</div>'
             )
         }
