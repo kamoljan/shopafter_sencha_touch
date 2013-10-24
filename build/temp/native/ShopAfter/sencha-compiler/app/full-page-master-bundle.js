58bafeb9035b98336f8ddd70327250a9e1f17e4d
Kamol Mavlonov <kamol@701search.com>
2013-10-24 11:08:18 +0800

    Cleaned up our git repo

--- build/temp/native/ShopAfter/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Users/kamol/work/sencha/touch-2.3.0/MyApps/ShopAfterM/app.js
--- phonegap/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/kamol/work/sencha/touch-2.3.0/MyApps/ShopAfterM/phonegap/platforms/android/bin/AndroidManifest.xml : \
-/Users/kamol/work/sencha/touch-2.3.0/MyApps/ShopAfterM/phonegap/platforms/android/AndroidManifest.xml \
